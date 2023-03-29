5fdd9c7dbc2e50170deb3e23e6895f59c0c60a02
kha <khalinux@naver.com>
2023-03-29 16:35:12 +0900

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_version = "<=1.4"
+
+  required_providers {
+    aws = "~> 4.6.0"
+  }
+}
+
+provider "aws" {
+    region = "ap-northeast-2"
+}
+
+resource "aws_key_pair" "myweb" {
+  key_name = "myweb"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_security_group" "ssh" {
+  name = "allow_ssh_from_all"
+  description = "Allow SSH port from all"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_security_group" "default" {
+  name = "default"
+}
+
+resource "aws_instance" "web" {
+  ami = "ami-0a93a08544874b3b7" # amzn2-ami-hvm-2.0.20200207.1-x86_64-gp2
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.myweb.key_name
+  vpc_security_group_ids = [
+    aws_security_group.ssh.id,
+    data.aws_security_group.default.id
+  ]
+}
